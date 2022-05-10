4711e7d3d54afe3b707646e5856dea3be4e6283a
BuildTools <unconfigured@null.spigotmc.org>
2022-05-10 17:00:19 -0500

    Smart git added

---
+++ SmartGit.sh
@@ -0,0 +1,8 @@
+cd Portfolio Website/
+git add --all
+echo "------------In Progress ----------"
+git commit -m"Repo Updated"
+echo "Git commited successfully"
+echo "Pushing Repo"
+echo "Provide Credentials for Gitthub Glitch3086"
+git push
